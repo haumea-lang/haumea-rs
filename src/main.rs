37616ab94439e06bc6b987573dcdbd50cc06f78b
BookOwl <stanleybookowl@gmail.com>
2016-10-19 16:37:29 -0400

    Add larger test input for the test program

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 extern crate haumea;
 
 fn main() {
-    let scanner = haumea::scanner::Scanner::new("if (1+123)*3 == x then foo()");
+    let source = "
+    to factorial with (n is an Integer, returns an Integer) do
+        if n = 0 then do
+            return 1
+        end
+        else do
+            return n * factorial(n - 1)
+        end
+    end";
+    let scanner = haumea::scanner::Scanner::new(source);
     println!("Starting to scan");
     for token in scanner {
         println!("{:?}", token);
