4f41430356960feb687e3f27b18da69951807911
BookOwl <stanleybookowl@gmail.com>
2016-10-30 18:41:32 -0400

    Fix indentation

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::io::prelude::*;
 
 fn main() {
     let mut source = String::new();
-	let mut stdin = io::stdin();
-	stdin.read_to_string(&mut source).expect("Must provide input");
+    let mut stdin = io::stdin();
+    stdin.read_to_string(&mut source).expect("Must provide input");
     let scanner = haumea::scanner::Scanner::new(&source);
-	let ast = haumea::parser::parse(scanner);
+    let ast = haumea::parser::parse(scanner);
     //println!("{:?}", ast);
-	let mut out = String::new();
-	haumea::codegen::compile_ast(&mut out, ast);
-	println!("{}", out);
+    let mut out = String::new();
+    haumea::codegen::compile_ast(&mut out, ast);
+    println!("{}", out);
 }
