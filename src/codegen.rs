a6d3a33fd31b4f6e52b0ffd44c66dbc50335c564
BookOwl <stanleybookowl@gmail.com>
2016-11-01 10:05:00 -0400

    Fix some final indentation glitches

--- src/codegen.rs
+++ src/codegen.rs
@@ -48,9 +48,9 @@ fn compile_function(mut out: &mut String, func: parser::Function) {
         }
     }
     out.push_str(") ");
-        out.push_str("{\n");
+    out.push_str("{\n");
     compile_statement(&mut out, func.code, 1);
-        out.push_str("\n}\n");
+    out.push_str("\n}\n");
 }
 
 /// Compiles a statement
